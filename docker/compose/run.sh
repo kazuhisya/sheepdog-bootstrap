9648ac4b349dca3e7b60b23a3fb891d791f007ec
Kazuhisa Hara <khara@sios.com>
2017-08-21 10:16:13 +0900

    Docker: added volume settings
    Signed-off-by: Kazuhisa Hara <khara@sios.com>

--- docker/compose/run.sh
+++ docker/compose/run.sh
@@ -4,4 +4,5 @@ until echo srvr | nc -w 1 zoo1 2181; do
     >&2 echo "zookeeper is not yet available - waiting..."
     sleep 1
 done
-/usr/sbin/sheep --cluster zookeeper:${ZOO_SERVERS} --log dst=syslog --upgrade ${SHEEPSTORE} -p ${SHEEPPORT} -n -f
+mkdir -p ${SHEEPSTORE}/${HOSTNAME}
+/usr/sbin/sheep --cluster zookeeper:${ZOO_SERVERS} --log dst=syslog --upgrade ${SHEEPSTORE}/${HOSTNAME} -p ${SHEEPPORT} -n -f
